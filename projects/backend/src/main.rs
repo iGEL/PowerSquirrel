4b73f127125f8dbc6fbf4f7452266d7bbf87da82
Johannes Barre <igel@igels.net>
2024-12-22 23:33:07 +0100

    Add weather

--- projects/backend/src/main.rs
+++ projects/backend/src/main.rs
@@ -1,23 +1,26 @@
 mod geocode;
 mod sun;
+mod weather;
 
 use geocode::geocode_zip;
 use geocode::Location;
 use sun::sun_position;
-use chrono::{TimeZone, Utc};
+use weather::fetch_weather;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let zip = "12207";
     let country = "Germany";
-    let date_time = Utc.with_ymd_and_hms(2024, 12, 1, 10, 13, 1).unwrap();
     let location = geocode_zip(zip, country).await?;
-    let sun = sun_position(date_time, location);
+    let weather = fetch_weather(&location).await?;
 
-    println!(
-        "Solar Altitude: {:.2}° Azimuth: {:.2}°",
-        sun.altitude,
-        sun.azimuth
-    );
+    for hour in weather.iter() {
+        let sun = sun_position(hour.time, &location);
+        println!(
+            "{} Temperature: {:.2}°, Clouds: {}% - Solar Altitude: {:.2}° Azimuth: {:.2}°",
+            hour.time, hour.temp, hour.clouds, sun.altitude, sun.azimuth
+        );
+    }
+
     Ok(())
 }
--- projects/backend/src/sun.rs
+++ projects/backend/src/sun.rs
@@ -1,10 +1,12 @@
 use crate::Location;
 
-pub fn sun_position(date_time: chrono::DateTime<chrono::Utc>, location: Location) -> SunPosition {
+pub fn sun_position(date_time: chrono::DateTime<chrono::Utc>, location: &Location) -> SunPosition {
     let pos = sun::pos(date_time.timestamp_millis(), location.lat, location.lon);
-    return SunPosition { altitude: pos.altitude.to_degrees(), azimuth: pos.azimuth.to_degrees() };
+    return SunPosition {
+        altitude: pos.altitude.to_degrees(),
+        azimuth: pos.azimuth.to_degrees(),
+    };
 }
-
 
 #[derive(Debug)]
 pub struct SunPosition {
